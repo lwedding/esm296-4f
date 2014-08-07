997295cf27c4fd63a43f353a17175408305e9ba5
bbest <bbest@nceas.ucsb.edu>
2014-08-07 12:07:40 -0700

    some R packages to explore for install

---
+++ admin/install_R_packages.R
@@ -0,0 +1,20 @@
+# for spatial packages, see http://cran.r-project.org/web/views/Spatial.html
+# for interactive examples, see http://shiny.rstudio.com/gallery/
+
+pkgs         = c('classInt',
+                 'devtools',
+                 'foreign',
+                 'gdistance','geonames','geosphere','ggplo2','ggmap','ggvis','geoR',
+                 'landsat','leafletR',
+                 'maps','mapdata','mapproj','maptools','marmap',
+                 'ncdf',
+                 'PBSmapping',
+                 'raster','rasterVis','RColorBrewer','rgeos','rgdal','RgoogleMaps','rworldmap','rworldxtra',
+                 'shapefiles','sp','spacetime','spatial.tools','spatstat')
+repos        = 'http://cran.rstudio.com'
+lib          = .libPaths()[1]
+dependencies = c('Depends', 'Imports', 'LinkingTo')
+install.packages(pkgs, lib, repos, dependencies=dependencies)
+
+pkgs_github  = c('rstudio/rmarkdown')
+for (p in pkgs_github) devtools::install_github(p)
